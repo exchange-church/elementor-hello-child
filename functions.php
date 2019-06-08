d71abb0cabb9fc328b5711861a4eceb4cc26bcab
Doug Penny <doug@exchangenc.org>
2019-06-08 16:53:27 -0400

    - Updated for new Elementor theme

--- functions.php
+++ functions.php
@@ -1,10 +1,13 @@
 <?php
 
 function child_theme_styles() {
-    $parent_style = 'elementor-hello-theme-style';
-
-    wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
-    wp_enqueue_style( 'child-theme-css', get_stylesheet_directory_uri() .'/style.css' , array($parent_style), wp_get_theme()->get('Version'));
+    $parent_style = 'hello-elementor';
+    wp_enqueue_style(
+        'elementor-hello-child',
+        get_stylesheet_directory_uri() .'/style.css',
+        array($parent_style),
+        wp_get_theme()->get('Version')
+    );
 }
 add_action( 'wp_enqueue_scripts', 'child_theme_styles' );
 
