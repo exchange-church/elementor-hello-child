6f706dd6f1d07b2bce0600044cfb6c35070d4205
Doug Penny <doug@exchangenc.org>
2019-12-03 11:22:06 -0500

    Add the dashicons for the front-end

--- functions.php
+++ functions.php
@@ -9,6 +9,12 @@ function child_theme_styles() {
         wp_get_theme()->get('Version')
     );
 }
+
+function load_dashicons_front_end() {
+    wp_enqueue_style( 'dashicons' );
+}
+
 add_action( 'wp_enqueue_scripts', 'child_theme_styles' );
+add_action( 'wp_enqueue_scripts', 'load_dashicons_front_end' );
 
 add_filter( 'elementor_hello_theme_add_woocommerce_support', '__return_false' );
